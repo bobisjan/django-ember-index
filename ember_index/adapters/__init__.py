830046d2f6548a9e8158234299d5f39739cb4665
Jan Bobisud <me@bobisjan.com>
2015-05-08 17:47:38 +0200

    Add docstring

--- ember_index/adapters/__init__.py
+++ ember_index/adapters/__init__.py
@@ -1,3 +1,10 @@
+'''A module with index adapters.
+
+An adapter has to implement `index_for(self, key)` method,
+which returns appropriate index as string or None if it does not exist.
+
+'''
+
 from .redis import RedisAdapter
 
 
